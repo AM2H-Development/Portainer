c87524b2f01740d7372ed357d9bbc86c42c22d90
martinhetschel <martin.hetschel@mach1kart.de>
2020-03-22 12:19:12 +0100

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 # Install by typing:
 # wget -q -O - https://raw.githubusercontent.com/AM2H-Development/Portainer/portainer.sh | bash
+docker stop portainer
+docker rm portainer
+docker volume create portainer_data
+docker pull portainer/portainer
+
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart=unless-stopped --name=portainer portainer/portainer
