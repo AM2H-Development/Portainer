048de4e64a07bd01eaf20c9436a1488ea343922f
martinhetschel <martin.hetschel@mach1kart.de>
2020-09-28 20:20:10 +0200

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -4,6 +4,6 @@
 docker stop portainer
 docker rm portainer
 docker volume create portainer_data
-docker pull portainer/portainer
+docker pull portainer/portainer-ce
 
-docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart=unless-stopped --name=portainer portainer/portainer
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data --restart=unless-stopped --name=portainer portainer/portainer-ce
