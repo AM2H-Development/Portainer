2acecada7bfdb8ac71058b1156ac3751074bbf07
martinhetschel <martin.hetschel@mach1kart.de>
2021-05-12 11:46:34 +0200

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -6,4 +6,4 @@ docker rm portainer
 docker volume create portainer_data
 docker pull portainer/portainer-ce
 
-docker run -d -p 8000:8000 -p 9091:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /var/certs:/certs --restart=unless-stopped --name=portainer portainer/portainer-ce --ssl --sslcert /certs/cert.pem --sslkey /certs/privkey.pem
+docker run -d -p 8000:8000 -p 9091:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /var/certs:/certs --restart=unless-stopped --name=portainer portainer/portainer-ce #--ssl --sslcert /certs/cert.pem --sslkey /certs/privkey.pem
