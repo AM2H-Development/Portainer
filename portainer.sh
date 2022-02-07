b4aff9943ee7e96ef5bb5c209933ccdb3391cc8c
jq7140 <am@clicca.de>
2022-02-07 19:10:50 +0100

    V 2.11.1

--- portainer.sh
+++ portainer.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-# Install by typing:
-# wget -q -O - https://raw.githubusercontent.com/AM2H-Development/Portainer/master/portainer.sh | bash
 docker stop portainer
 docker rm portainer
 docker volume create portainer_data
-# docker pull portainer/portainer-ce:2.5.1-alpine
-
-docker run -d -p 8000:8000 -p 9091:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /var/certs:/certs --restart=unless-stopped --name=portainer portainer/portainer-ce:2.5.1-alpine --ssl --sslcert /certs/cert.pem --sslkey /certs/privkey.pem
+docker run -d -p 8000:8000 -p 9091:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /var/certs:/certs --restart=unless-stopped --name=portainer portainer/portainer-ce:2.11.1-alpine --ssl --sslcert /certs/cert.pem --sslkey /certs/privkey.pem
