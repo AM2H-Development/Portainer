bda34ce82a42513a9919fa101bf42d6d97c2f743
martinhetschel <martin.hetschel@mach1kart.de>
2024-06-15 17:10:41 +0200

    Delete portainer.sh

--- portainer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker stop portainer
-docker rm portainer
-docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9091:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -v /var/certs:/certs --restart=unless-stopped --name=portainer portainer/portainer-ce:2.11.1-alpine --ssl --sslcert /certs/cert.pem --sslkey /certs/privkey.pem
